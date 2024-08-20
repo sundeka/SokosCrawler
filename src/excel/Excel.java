75f477efcbaaa5f447989d27f7cd8e819aceb755
Kalle Sundell <sundellkalle@gmail.com>
2024-08-20 17:13:58 +0300

    init `Excel` class for file operations

---
+++ src/excel/Excel.java
@@ -0,0 +1,13 @@
+package excel;
+
+public class Excel {
+	
+	/**
+	 * 
+	 * @param fileName: the suffix-less filename of the file e.g. "snacks"
+	 * @return boolean
+	 */
+	public boolean fileExists(String fileName) {
+		return false;
+	}
+}
