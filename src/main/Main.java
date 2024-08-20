e90bc201da8d9fec81ddc0a66f5f58fce5edf9f2
Kalle Sundell <sundellkalle@gmail.com>
2024-08-20 17:14:10 +0300

    hone main logic further

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import crawler.Crawler;
+import excel.Excel;
 import exceptions.CrawlerException;
 import sokos.SideMenuItem;
 import sokos.Sokos;
@@ -15,12 +16,16 @@ public class Main {
 	public static void main(String[] args) throws CrawlerException {
 		Crawler crawler = new Crawler(logger);
 		Sokos sokos = new Sokos();
+		Excel excel = new Excel();
 		if (crawler.openSokos()) {
 			ArrayList<SideMenuItem> menuItems = sokos.getSideMenuItems();
 			for (SideMenuItem menuItem : menuItems) {
-				System.out.println(menuItem.getSideBarTitle());
-				System.out.println(menuItem.getExcelTitle());
-				System.out.println("---");
+				if (!excel.fileExists(menuItem.getExcelTitle())) {
+					// TODO: Crawler stuff
+					continue;
+				} else {
+					logger.info("Already handled Excel file '" + menuItem.getExcelTitle() + "' on the crashed run, continuing to the next one.");
+				}
 			}
 		}	
 	}
